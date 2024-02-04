b7ab1376c1ab56a2e7d5d1a873ed7fb4d246fe1b
youngbin03 <87307678+youngbin03@users.noreply.github.com>
2024-02-05 08:59:05 +0900

    Update 7576-syb.cpp

--- 7576-syb.cpp
+++ 7576-syb.cpp
@@ -52,7 +52,7 @@ int main() {
     /* 그래프 정보 입력*/
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            scanf("%d", &graph[i][j]);
+            cin >> graph[i][j];
             if (graph[i][j] == 1) { // 익은토마토(1) -> 큐
                 q.push({ i, j });
             }
@@ -67,7 +67,7 @@ int main() {
         for (int j = 0; j < m; j++) {
             // 익지않은 토마토(0)가 존재할 경우
             if (graph[i][j] == 0) { 
-                printf("-1\n");
+                cout << -1 ;
                 return 0;
             }
             // 토마토는 다 익었는데, 얼마만에 익었는지?
@@ -76,6 +76,6 @@ int main() {
             }
         }
     }
-    printf("%d\n", result-1);
+    cout << result - 1;
     return 0;
 }
